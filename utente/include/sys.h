fefe1f62a6cff6fa0cef31194047fc8d2759e92d
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2008-11-10 09:54:48 +0000

    facciamo in modo che i tipi delle primitive in utente.s siano automaticamente quelli di sistema.s

--- utente/include/sys.h
+++ utente/include/sys.h
@@ -5,7 +5,8 @@
 #ifndef SYS_H_
 #define SYS_H_
 
-#include "tipo.h"
+#include <tipo.h>
+#include <costanti.h>
 
 extern "C" natl activate_p(void f(int), int a, natl prio, natl liv);
 extern "C" void terminate_p();
@@ -22,15 +23,6 @@ extern "C" void readse_ln(natl serial, natb vetti[], natl &quanti, natb &errore)
 extern "C" void writese_0(natl serial, natb vetto[], natl &quanti);
 extern "C" void writese_n(natl serial, natb vetto[], natl quanti);
 
-
-const unsigned int LOG_MSG_SIZE = 72;
-enum log_sev { LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERR };
-
-struct log_msg {
-	log_sev sev;
-	short  identifier;
-	char msg[LOG_MSG_SIZE];
-};
 extern "C" void readlog(log_msg& m);
 extern "C" void log(log_sev sev, const char* msg, int quanti);
 
@@ -40,9 +32,6 @@ extern "C" void iniconsole(natb cc);
 extern "C" void readconsole(char* buff, int& quanti);
 extern "C" void writeconsole(const char* buff);
 
-// Costanti
-//
-const int LIV_SISTEMA = 0, LIV_UTENTE = 3;
 
 #endif
 
