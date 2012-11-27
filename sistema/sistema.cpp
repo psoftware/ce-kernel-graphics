5468ec1d37c5540382b47d77c915e4bcdebda3b1
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2012-11-27 16:56:47 +0000

    tolleriamo i pf dal modulo sistema

--- sistema/sistema.cpp
+++ sistema/sistema.cpp
@@ -527,12 +527,11 @@ extern "C" void c_routine_pf(	// [6.4]
 	//   dalle primitie di nucleo (vedi [6.5]), quindi, se cio' si e' verificato, 
 	//   si tratta di un bug
 	if (eip < fine_codice_sistema || errore.res == 1) {
-		flog(LOG_ERR, "eip: %x, page fault a %x: %s, %s, %s, %s", eip, ind_virt_non_tradotto,
+		flog(LOG_WARN, "eip: %x, page fault a %x: %s, %s, %s, %s", eip, ind_virt_non_tradotto,
 			errore.prot  ? "protezione"	: "pag/tab assente",
 			errore.write ? "scrittura"	: "lettura",
 			errore.user  ? "da utente"	: "da sistema",
 			errore.res   ? "bit riservato"	: "");
-		panic(errore.res ? "bit riservato = 1" : "page fault dal modulo sistema");
 	}
 	// *)
 	
