c957ab0b6b17df410cc2fd2b23a66bfc2a057253
Yuri Iozzelli <y.iozzelli@gmail.com>
2013-11-30 21:35:54 +0100

    implemented PCI,APIC and timer support

--- sistema/sistema64.cpp
+++ sistema/sistema64.cpp
@@ -57,7 +57,7 @@ extern "C" void do_log(log_sev sev, const char* buf, natl quanti)
 	const char* lev[] = { "DBG", "INF", "WRN", "ERR", "USR" };
 	if (sev > MAX_LOG) {
 		flog(LOG_WARN, "Livello di log errato: %d", sev);
-		//abort_p();
+		//panic("abort");
 	}
 	const natb* l = (const natb*)lev[sev];
 	while (*l)
@@ -438,6 +438,163 @@ extern "C" void invalida_TLB();
 
 
 // )
+const natl MAX_IRQ  = 24;
+// )
+
+
+/////////////////////////////////////////////////////////////////////////////////
+//                    SUPPORTO PCI                                             //
+/////////////////////////////////////////////////////////////////////////////////
+const ioaddr PCI_CAP = 0x0CF8;
+const ioaddr PCI_CDP = 0x0CFC;
+
+extern "C" void inputb(ioaddr reg, natb &a);	// [9.3.1]
+extern "C" void outputb(natb a, ioaddr reg);	// [9.3.1]
+// (*
+extern "C" void inputw(ioaddr reg, natw &a);	
+extern "C" void outputw(natw a, ioaddr reg);
+extern "C" void inputl(ioaddr reg, natl &a);	
+extern "C" void outputl(natl a, ioaddr reg);
+// *)
+
+natl make_CAP(natw w, natb off)
+{
+	return 0x80000000 | (w << 8) | (off & 0xFC);
+}
+
+natb pci_read_confb(natw w, natb off)
+{
+	natl l = make_CAP(w, off);
+	outputl(l, PCI_CAP);
+	natb ret;
+	inputb(PCI_CDP + (off & 0x03), ret);
+	return ret;
+}
+
+natw pci_read_confw(natw w, natb off)
+{
+	natl l = make_CAP(w, off);
+	outputl(l, PCI_CAP);
+	natw ret;
+	inputw(PCI_CDP + (off & 0x03), ret);
+	return ret;
+}
+
+natl pci_read_confl(natw w, natb off)
+{
+	natl l = make_CAP(w, off);
+	outputl(l, PCI_CAP);
+	natl ret;
+	inputl(PCI_CDP, ret);
+	return ret;
+}
+
+void pci_write_confb(natw w, natb off, natb data)
+{
+	natl l = make_CAP(w, off);
+	outputl(l, PCI_CAP);
+	outputb(data, PCI_CDP + (off & 0x03));
+}
+
+void pci_write_confw(natw w, natb off, natw data)
+{
+	natl l = make_CAP(w, off);
+	outputl(l, PCI_CAP);
+	outputw(data, PCI_CDP + (off & 0x03));
+}
+
+void pci_write_confl(natw w, natb off, natl data)
+{
+	natl l = make_CAP(w, off);
+	outputl(l, PCI_CAP);
+	outputl(data, PCI_CDP);
+}
+
+bool pci_find_dev(natw& w, natw devID, natw vendID)
+{
+	for( ; w != 0xFFFF; w++) {
+		natw work;
+
+		if ( (work = pci_read_confw(w, 0)) == 0xFFFF ) 
+			continue;
+		if ( work == vendID && pci_read_confw(w, 2) == devID) 
+			return true;
+	}
+	return false;
+}
+
+bool pci_find_class(natw& w, natb code[])
+{
+	for ( ; w != 0xFFFF; w++) {
+		if (pci_read_confw(w, 0) == 0xFFFF)
+			continue;
+		natb work[3];
+		natl i;
+		for (i = 0; i < 3; i++) {
+			work[i] = pci_read_confb(w, 2 * 4 + i + 1);
+			if (code[i] != 0xFF && code[i] != work[i])
+				break;
+		}
+		if (i == 3) {
+			for (i = 0; i < 3; i++)
+				code[i] = work[i];
+			return true;
+		}
+	} 
+	return false;
+}
+
+extern "C" natl c_pci_find(natl code, natw i)
+{
+	natb* pcode = (natb*)&code;
+	natw w, j = 0;
+	for(w = 0; w != 0xFFFF; w++) {
+		if (! pci_find_class(w, pcode)) 
+			return 0xFFFFFFFF;
+		if (j == i)
+			return w;
+		j++;
+	} 
+	return 0xFFFFFFFF;
+}
+
+extern "C" natl c_pci_read(natw l, natw regn, natl size)
+{
+	natl res;
+	switch (size) {
+	case 1:
+		res = pci_read_confb(l, regn);
+		break;
+	case 2:
+		res = pci_read_confw(l, regn);
+		break;
+	case 4:
+		res = pci_read_confl(l, regn);
+		break;
+	default:
+		flog(LOG_WARN, "pci_read(%x, %d, %d): parametro errato", l, regn, size);
+		panic("abort");
+	}
+	return res;
+}
+
+extern "C" void c_pci_write(natw l, natw regn, natl res, natl size)
+{
+	switch (size) {
+	case 1:
+		pci_write_confb(l, regn, res);
+		break;
+	case 2:
+		pci_write_confw(l, regn, res);
+		break;
+	case 4:
+		pci_write_confl(l, regn, res);
+		break;
+	default:
+		flog(LOG_WARN, "pci_write(%x, %d, %d, %d): parametro errato", l, regn, res, size);
+		panic("abort");
+	}
+}
 /////////////////////////////////////////////////////////////////////////////////
 //                    SUPPORTO PCI                                             //
 /////////////////////////////////////////////////////////////////////////////////
@@ -457,47 +614,212 @@ bool crea_finestra_PCI(addr pml4)
 			BIT_RW | BIT_PCD);
 }
 
-//// ( [P_IOAPIC]
-#include "apic.h"
+natb pci_getbus(natw l)
+{
+	return l >> 8;
+}
 
+natb pci_getdev(natw l)
+{
+	return (l & 0x00FF) >> 3;
+}
+
+natb pci_getfun(natw l)
+{
+	return l & 0x0007;
+}
+// )
+
+// ( [P_IOAPIC]
+
+// parte piu' significativa di una redirection table entry
+const natl IOAPIC_DEST_MSK = 0xFF000000; // destination field mask
+const natl IOAPIC_DEST_SHF = 24;	 // destination field shift
+// parte meno significativa di una redirection table entry
+const natl IOAPIC_MIRQ_BIT = (1U << 16); // mask irq bit
+const natl IOAPIC_TRGM_BIT = (1U << 15); // trigger mode (1=level, 0=edge)
+const natl IOAPIC_IPOL_BIT = (1U << 13); // interrupt polarity (0=high, 1=low)
+const natl IOAPIC_DSTM_BIT = (1U << 11); // destination mode (0=physical, 1=logical)
+const natl IOAPIC_DELM_MSK = 0x00000700; // delivery mode field mask (000=fixed)
+const natl IOAPIC_DELM_SHF = 8;		 // delivery mode field shift
+const natl IOAPIC_VECT_MSK = 0x000000FF; // vector field mask
+const natl IOAPIC_VECT_SHF = 0;		 // vector field shift
+
+struct ioapic_des {
+	natl* IOREGSEL;
+	natl* IOWIN;
+	natl* EOI;
+	natb  RTO;	// Redirection Table Offset
+};
+
+extern "C"  ioapic_des ioapic;
+
+natl ioapic_in(natb off)
+{
+	*ioapic.IOREGSEL = off;
+	return *ioapic.IOWIN;
+}
+
+void ioapic_out(natb off, natl v)
+{
+	*ioapic.IOREGSEL = off;
+	*ioapic.IOWIN = v;
+}
+
+natl ioapic_read_rth(natb irq)
+{
+	return ioapic_in(ioapic.RTO + irq * 2 + 1);
+}
+
+void ioapic_write_rth(natb irq, natl w)
+{
+	ioapic_out(ioapic.RTO + irq * 2 + 1, w);
+}
+
+natl ioapic_read_rtl(natb irq)
+{
+	return ioapic_in(ioapic.RTO + irq * 2);
+}
+
+void ioapic_write_rtl(natb irq, natl w)
+{
+	ioapic_out(ioapic.RTO + irq * 2, w);
+}
+
+void ioapic_set_VECT(natl irq, natb vec)
+{
+	natl work = ioapic_read_rtl(irq);
+	work = (work & ~IOAPIC_VECT_MSK) | (vec << IOAPIC_VECT_SHF);
+	ioapic_write_rtl(irq, work);
+}
+
+void ioapic_set_MIRQ(natl irq, bool v)
+{
+	natl work = ioapic_read_rtl(irq);
+	if (v)
+		work |= IOAPIC_MIRQ_BIT;
+	else
+		work &= ~IOAPIC_MIRQ_BIT;
+	ioapic_write_rtl(irq, work);
+}
+
+extern "C" void ioapic_mask(natl irq)
+{
+	ioapic_set_MIRQ(irq, true);
+}
+
+extern "C" void ioapic_unmask(natl irq)
+{
+	ioapic_set_MIRQ(irq, false);
+}
+
+void ioapic_set_TRGM(natl irq, bool v)
+{
+	natl work = ioapic_read_rtl(irq);
+	if (v)
+		work |= IOAPIC_TRGM_BIT;
+	else
+		work &= ~IOAPIC_TRGM_BIT;
+	ioapic_write_rtl(irq, work);
+}
+
+
+const natw PIIX3_VENDOR_ID = 0x8086;
+const natw PIIX3_DEVICE_ID = 0x7000;
+const natb PIIX3_APICBASE = 0x80;
+const natb PIIX3_XBCS = 0x4e;
+const natl PIIX3_XBCS_ENABLE = (1U << 8);
+
+natl apicbase_getxy(natl apicbase)
+{
+	return (apicbase & 0x01F) << 10U;
+}
+
+extern "C" void disable_8259();
 bool ioapic_init()
 {
-	if (!apic_init())
-			return false;
-	natq offset = inizio_pci_condiviso - (natb*)PCI_startmem;
-	IOREGSEL = (natl*)((natb*)IOREGSEL + offset);
-	IOWIN = (natl*)((natb*)IOWIN + offset);
-	IOAPIC_EOIR = (natl*)((natb*)IOAPIC_EOIR + offset);
-	apic_reset();
-	apic_set_VECT(0, VETT_0);
-	apic_set_VECT(1, VETT_1);
-	apic_set_VECT(2, VETT_2);
-	apic_set_VECT(3, VETT_3);
-	apic_set_VECT(4, VETT_4);
-	apic_set_VECT(5, VETT_5);
-	apic_set_VECT(6, VETT_6);
-	apic_set_VECT(7, VETT_7);
-	apic_set_VECT(8, VETT_8);
-	apic_set_VECT(9, VETT_9);
-	apic_set_VECT(10, VETT_10);
-	apic_set_VECT(11, VETT_11);
-	apic_set_VECT(12, VETT_12);
-	apic_set_VECT(13, VETT_13);
-	apic_set_VECT(14, VETT_14);
-	apic_set_VECT(15, VETT_15);
-	apic_set_VECT(16, VETT_16);
-	apic_set_VECT(17, VETT_17);
-	apic_set_VECT(18, VETT_18);
-	apic_set_VECT(19, VETT_19);
-	apic_set_VECT(20, VETT_20);
-	apic_set_VECT(21, VETT_21);
-	apic_set_VECT(22, VETT_22);
-	apic_set_VECT(23, VETT_23);
+	natw l = 0;
+	// trovare il PIIX3 e
+	if (!pci_find_dev(l, PIIX3_DEVICE_ID, PIIX3_VENDOR_ID)) {
+		flog(LOG_WARN, "PIIX3 non trovato");
+		return false;
+	}
+	flog(LOG_DEBUG, "PIIX3: trovato a %2x.%2x.%2x",
+			pci_getbus(l), pci_getdev(l), pci_getfun(l));
+	// 	inizializzare IOREGSEL e IOWIN
+	natb apicbase;
+	apicbase = pci_read_confb(l, PIIX3_APICBASE);
+	natq tmp_IOREGSEL = (natq)ioapic.IOREGSEL | apicbase_getxy(apicbase);
+	natq tmp_IOWIN    = (natq)ioapic.IOWIN    | apicbase_getxy(apicbase);
+	// 	trasformiamo gli indirizzi fisici in virtuali
+	ioapic.IOREGSEL = (natl*)(tmp_IOREGSEL - (natq)PCI_startmem + (natq)inizio_pci_condiviso);
+	ioapic.IOWIN    = (natl*)(tmp_IOWIN    - (natq)PCI_startmem + (natq)inizio_pci_condiviso);
+	ioapic.EOI	= (natl*)((natq)ioapic.EOI - (natq)PCI_startmem + (natq)inizio_pci_condiviso);
+	flog(LOG_DEBUG, "IOAPIC: ioregsel %p, iowin %p", ioapic.IOREGSEL, ioapic.IOWIN);
+	// 	abilitare il /CS per l'IOAPIC
+	natl xbcs;
+	xbcs = pci_read_confl(l, PIIX3_XBCS);
+	xbcs |= PIIX3_XBCS_ENABLE;
+	pci_write_confl(l, PIIX3_XBCS, xbcs);
+	disable_8259();
+	// riempire la redirection table
+	for (natb i = 0; i < MAX_IRQ; i++) {
+		ioapic_write_rth(i, 0);
+		ioapic_write_rtl(i, IOAPIC_MIRQ_BIT | IOAPIC_TRGM_BIT);
+	}
+	ioapic_set_VECT(0, VETT_0);	ioapic_set_TRGM(0, false);
+	ioapic_set_VECT(1, VETT_1);
+	ioapic_set_VECT(2, VETT_2);	ioapic_set_TRGM(2, false);
+	ioapic_set_VECT(3, VETT_3);
+	ioapic_set_VECT(4, VETT_4);
+	ioapic_set_VECT(5, VETT_5);
+	ioapic_set_VECT(6, VETT_6);
+	ioapic_set_VECT(7, VETT_7);
+	ioapic_set_VECT(8, VETT_8);
+	ioapic_set_VECT(9, VETT_9);
+	ioapic_set_VECT(10, VETT_10);
+	ioapic_set_VECT(11, VETT_11);
+	ioapic_set_VECT(12, VETT_12);
+	ioapic_set_VECT(13, VETT_13);
+	ioapic_set_VECT(14, VETT_14);
+	ioapic_set_VECT(15, VETT_15);
+	ioapic_set_VECT(16, VETT_16);
+	ioapic_set_VECT(17, VETT_17);
+	ioapic_set_VECT(18, VETT_18);
+	ioapic_set_VECT(19, VETT_19);
+	ioapic_set_VECT(20, VETT_20);
+	ioapic_set_VECT(21, VETT_21);
+	ioapic_set_VECT(22, VETT_22);
+	ioapic_set_VECT(23, VETT_23);
 	return true;
 }
-//
-//// )
 
+extern "C" void ioapic_reset()
+{
+	for (natb i = 0; i < MAX_IRQ; i++) {
+		ioapic_write_rth(i, 0);
+		ioapic_write_rtl(i, IOAPIC_MIRQ_BIT | IOAPIC_TRGM_BIT);
+	}
+	natw l = 0;
+	pci_find_dev(l, PIIX3_DEVICE_ID, PIIX3_VENDOR_ID);
+	natl xbcs;
+	xbcs = pci_read_confl(l, PIIX3_XBCS);
+	xbcs &= ~PIIX3_XBCS_ENABLE;
+	pci_write_confl(l, PIIX3_XBCS, xbcs);
+}
+
+// )
+
+// timer
+extern natl ticks;
+extern natl clocks_per_usec;
+extern "C" void attiva_timer(natl count);
+const natl DELAY = 59659;
+extern "C" void c_driver_td()
+{
+	flog(LOG_INFO,"timer");
+
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 //                          TESTS                                            //
@@ -506,7 +828,6 @@ void test_int()
 {
 	asm(
 		"int $0xf0 \n"
-		"int $0xd1 \n"
 	   );
 	
 	flog(LOG_DEBUG, "test_int --->  1");
@@ -522,7 +843,6 @@ void test_mapping(addr testpml4)
 
 extern "C" void cmain ()
 {
-	
 	flog(LOG_INFO, "Nucleo di Calcolatori Elettronici, v4.02");
 
 	// (* Assegna allo heap di sistema HEAP_SIZE byte nel primo MiB
@@ -556,7 +876,14 @@ extern "C" void cmain ()
 	ioapic_init();
 	asm("sti");
 	flog(LOG_INFO, "APIC inizializzato e interruzioni abilitate!");
-	while(true);
+
+	attiva_timer(DELAY);
+	flog(LOG_INFO, "timer attivato!");
+	
+	while(true)
+	{
+	
+	}
 
 	test_mapping(testpml4);
 	test_int();
