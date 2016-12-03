f6eb7464264924e21671e6c675dc8b50641c01d9
Antonio le Caldare <antonio.lecaldare@gmail.com>
2016-12-03 02:13:43 +0100

    windows user lib: added u_window class that manages window and child objects. updated hello1.in example using u_window

--- utente/include/winobj.h
+++ utente/include/winobj.h
@@ -45,3 +45,57 @@ class u_label : public u_windowObject
 	}
 };
 
+
+const int MAX_USER_OBJECTS=20;
+class u_window
+{
+private:
+	int w_id;
+	int objs_ident[MAX_USER_OBJECTS];
+	u_windowObject * objs[MAX_USER_OBJECTS];
+	int objs_count;
+
+public:
+	u_window(int size_x, int size_y, int pos_x, int pos_y)
+	{
+		w_id = crea_finestra(size_x, size_y, pos_x, pos_y);
+		objs_count=0;
+	}
+
+	void show(bool async=false)
+	{
+		visualizza_finestra(w_id, true);
+	}
+
+	bool add_object(u_windowObject* obj)
+	{
+		//ho un limite MAX_USER_OBJECTS da rispettare
+		if(objs_count>=MAX_USER_OBJECTS)
+			return false;
+
+		//controllo che l'oggetto non sia già stato aggiunto
+		for(int i=0; i<objs_count; i++)
+			if(this->objs[i]==obj)
+				return false;
+
+		//mi mantengo solo il puntatore all'oggetto, così che l'utente possa modificarlo senza chiamare funzioni
+		this->objs[this->objs_count] = obj;
+
+		//uso la primitiva per creare l'oggetto e mi mantengo l'identificatore per modifiche future
+		this->objs_ident[this->objs_count] = crea_oggetto(this->w_id, obj);
+		objs_count++;
+
+		return true;
+	}
+
+	bool update_object(u_windowObject* obj, bool async=false)
+	{
+		for(int i=0; i<objs_count; i++)
+			if(this->objs[i]==obj)
+			{
+				aggiorna_oggetto(this->w_id, this->objs_ident[i], this->objs[i], async);
+				return true;
+			}
+		return false;
+	}
+};
