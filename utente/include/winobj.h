7e33f255a59af6c320214f60e4ea25aebf58496f
Antonio le Caldare <antonio.lecaldare@gmail.com>
2016-12-04 19:08:16 +0100

    winobj: added internal object focus

--- utente/include/winobj.h
+++ utente/include/winobj.h
@@ -12,11 +12,14 @@ private:
 	u_windowObject * objs[MAX_USER_OBJECTS];
 	int objs_count;
 
+	int internal_focus;
+
 public:
 	u_window(int size_x, int size_y, int pos_x, int pos_y)
 	{
 		w_id = crea_finestra(size_x, size_y, pos_x, pos_y);
 		objs_count=0;
+		internal_focus=-1;
 	}
 
 	void show(bool async=false)
@@ -62,14 +65,26 @@ public:
 		if(new_event.type==NOEVENT)
 			return;
 		flog(LOG_INFO, "process_event: nuovo evento di tipo %d prelevato, rel_x %d rel_y %d delta_z", new_event.type, new_event.rel_x, new_event.rel_y);
-		for(int i=0; i<this->objs_count; i++)
-			if(new_event.rel_x > objs[i]->pos_x && new_event.rel_x < objs[i]->pos_x + objs[i]->size_x &&
-				new_event.rel_y > objs[i]->pos_y && new_event.rel_y < objs[i]->pos_y + objs[i]->size_y)
-			{
-				objs[i]->process_event(new_event.type);
-				update_object(objs[i]);
-				flog(LOG_INFO, "process_event: BECCATO %d", LOG_INFO);
-			}
+
+		//eventi con target x,y
+		int i;
+		if(new_event.type==USER_EVENT_MOUSEZ || new_event.type==USER_EVENT_MOUSEUP || new_event.type==USER_EVENT_MOUSEDOWN)
+			for(i=0; i<this->objs_count; i++)
+				if(new_event.rel_x > objs[i]->pos_x && new_event.rel_x < objs[i]->pos_x + objs[i]->size_x &&
+					new_event.rel_y > objs[i]->pos_y && new_event.rel_y < objs[i]->pos_y + objs[i]->size_y)
+				{
+					internal_focus=i;
+					objs[i]->process_event(new_event.type);
+					update_object(objs[i]);
+					flog(LOG_INFO, "process_event: BECCATO %d", LOG_INFO);
+				}
+
+		//se l'evento con coordinate non ha centrato oggetti, allora devo togliere il focus
+		if(i==this->objs_count)
+			internal_focus=-1;
+
+		//eventi senza target x,y, quindi applicati su internal_focus
+		//if(new_event.type==USER_EVENT_KEYBOARDPRESS)
 	}
 };
 
