b8bd6c99916ec538da03dafd8727fad4843a0198
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2017-06-29 17:21:49 +0200

    v5.8

--- sistema/sistema.cpp
+++ sistema/sistema.cpp
@@ -1616,7 +1616,7 @@ extern "C" void cmain()
 	esecuzione = &init;
 	// *)
 
-	flog(LOG_INFO, "Nucleo di Calcolatori Elettronici, v5.7");
+	flog(LOG_INFO, "Nucleo di Calcolatori Elettronici, v5.8");
 	init_gdt();
 	flog(LOG_INFO, "gdt inizializzata");
 
